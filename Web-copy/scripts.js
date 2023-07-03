3dc62af581ff7ab0a8fd2aec0ce48c475cc4eb5e
Miguel Gomez <138181447+ForMykee@users.noreply.github.com>
2023-07-02 21:55:06 -0600

    Add files via upload

--- Web-copy/scripts.js
+++ Web-copy/scripts.js
@@ -1,12 +1,1 @@
-const DarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-function toggleDM() {
-  const body = document.body;
-  body.classList.toggle("dark-mode");
-}
-
-window.onload = function() {
-  if (DarkMode) {
-    toggleDM();
-  }
-}
+// Obtener el elemento que desencadena la apertura del menú
